cae55a1c9fdb67f2b52769d1757e0e45a9cd4d81
Yoss <gironandrea544gmail.com>
2025-03-05 19:34:01 -0600

    Primero commit y tambien push

---
+++ Ejercicio1-aspx/New1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ejercicio1_aspx
+{
+	public partial class New1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btn_Confir_Click(object sender, EventArgs e)
+        {
+			//Save the cookies
+			Response.Cookies["ddlCategory"].Value = ddlCategory.SelectedValue;
+			Response.Cookies["ddlSupplier"].Value = ddlSupplier.SelectedValue;
+			Response.Cookies["txtProduct"].Value = txtProduct.Text;
+			Response.Cookies["txtDescription"].Value = txtDescription.Text;
+			Response.Cookies["txtImage"].Value = txtImage.Text;
+			Response.Cookies["txtPrice"].Value = txtPrice.Text;
+			Response.Cookies["txtNumberInStock"].Value = txtNumberInStock.Text;
+			Response.Cookies["txtNumberOnOrder"].Value = txtNumberOnOrder.Text;
+			Response.Cookies["txtReorderLevel"].Value = txtReorderLevel.Text;
+			//Go to the confirmation page.
+			Response.Redirect("New2.aspx");
+		}
+    }
+}
